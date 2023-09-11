f7200d0ff5109dd2f5921ef8f8c75e841c7af5a7
Charlotte <bizelcharlotte@gmail.com>
2023-09-11 16:04:10 +0200

    ask for number done

--- src/example_package/app.py
+++ src/example_package/app.py
@@ -28,10 +28,23 @@ def show_items():
 
 def search_by_name():
     pass
+# 
+
+def ask_for_number() -> int:
+    choice :str = input("your choice: ")
+    if choice.isdigit():
+        return int(choice)
+    else:
+        print("error: not a number")
+        return -1
+
 
 def main():
+    choice : int
+
     print("welcome to passman !!".center(100,"-"))
 
+    choice : int
     while choice := -1 != 0 :
 
         print("""
@@ -42,7 +55,7 @@ def main():
         \r0 exit.      
         """)
 
-        choice = int(input("your choice:"))
+        choice = ask_for_number() #int(input("your choice:"))
 
         match choice:
             case 0:
@@ -54,11 +67,7 @@ def main():
             case 3:
                 login()
             case _: #le _: signifie TOUT les cas sauf ceux au dessu
-                print("erreur")
-
-
-
-
+                print("warning")
 
 # entry point
 if __name__ == "__main__":
