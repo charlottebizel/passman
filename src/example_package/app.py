b9ec744c12bfde2ad5e8c34a663fccd4a328a7d2
Charlotte <bizelcharlotte@gmail.com>
2023-09-12 14:48:46 +0200

    login search et list item done

--- src/example_package/app.py
+++ src/example_package/app.py
@@ -16,7 +16,6 @@ def create_user():
         notice("user created.")
 
 
-
 def remove_user():
     user_login = input("enter a user to delete: ")
 
@@ -28,9 +27,61 @@ def remove_user():
         else:
             error(" user not found ! ")
 
+def ask_for_number2() -> int:
+    choice :str = input("your choice: ")
+    if choice.isdigit():
+        return int(choice)
+    else:
+        error(" not a number")
+        return -1
 
 def login():
-    pass
+    print("test")
+    login = input("enter your user name : ")
+
+    if login in users.keys():
+        global active_vault
+        active_vault = users[login]
+        choice : int
+
+    print("welcome to your vault ".center(100,"-"))
+
+    choice : int
+    while choice := -1 != 0 :
+
+        print("""
+        \r1. create item
+        \r2. remove item
+        \r3. list items
+        \r4. show items
+        \r5. search by name
+        \r0 exit.      
+        """)
+
+        choice = ask_for_number2() #int(input("your choice:"))
+
+        match choice:
+            case 0:
+                exit()
+            case 1:
+                create_item()
+            case 2:
+                remove_item()
+            case 3:
+                list_items()
+            case 4 : 
+                show_items()
+            case 5 : 
+                search_by_name()
+            case 6 :
+                edit_items()
+            case _: #le _: signifie TOUT les cas sauf ceux au dessu
+                error("warning")   
+
+    else:
+        error("this user name does not exist")
+
+
 
 def create_item():
     pass
@@ -41,7 +92,10 @@ def remove_item():
 
 
 def list_items():
-    pass
+    for item in active_vault:
+        item_name,_,_ = item #_ means there is a variable but not used
+        #if you want to write you can
+        print("item_name")
 
 
 def show_items():
@@ -49,6 +103,15 @@ def show_items():
 
 
 def search_by_name():
+    query = input("what are you looking for?:")
+    for item in active_vault:
+        item_name,_,_ = item 
+        if item_name.startswith(query):
+            print(item_name)
+
+    pass
+
+def edit_items():
     pass
     """
     ask if the choice is a number and answer the choice if ok or -1
@@ -97,5 +160,6 @@ def main():
 if __name__ == "__main__":
     #some variables
     users: dict[str,list[tuple[str,str,str]]] = {} #key :str, value:list of tuples
-
+    active_vault:list[tuple[str,str,str]] = []
+    # variable pour l'apelle du vault dans login qui utile dans d'autre fonct
     main()
